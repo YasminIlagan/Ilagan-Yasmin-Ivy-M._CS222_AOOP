dd7bf2c3f5bc24dd40cc9382729ebd7321664107
YasminIlagan <158028352+YasminIlagan@users.noreply.github.com>
2025-02-11 12:48:34 +0800

    Update alternatingChars.cs

--- Laboratory Activities/Laboratory Activity 1- {10-02-25}/alternatingChars.cs
+++ Laboratory Activities/Laboratory Activity 1- {10-02-25}/alternatingChars.cs
@@ -1,1 +1,37 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("First character: ");
+        char firstChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Second character: ");
+        char secondChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Enter size: ");
+        int size = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < size; i++)
+        {
+          
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("-");
+            }
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(firstChar);
+            }
+            else
+            {
+                Console.WriteLine(secondChar);
+            }
+        }
+    }
+}
 
